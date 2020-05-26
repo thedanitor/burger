3b854c18fcabde77df01e3cb24d118a712773674
Dan Weikart <danmweikart@gmail.com>
2020-05-26 08:56:20 -0700

    updated connection.js

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
