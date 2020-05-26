c6c7f1c9f74710f4e3a6ba4ba706c4d12515e3e9
Dan Weikart <danmweikart@gmail.com>
2020-05-26 10:16:39 -0700

    Updated schema

--- db/burger_db.sql
+++ db/burger_db.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers (
+CREATE TABLE department (
 id INTEGER(11) AUTO_INCREMENT NOT NULL,
 burger_name VARCHAR(30) NOT NULL,
 devoured BOOLEAN NOT NULL,
