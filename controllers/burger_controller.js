fddbf073e242f578d5ff4ae79cee49f8cd0d2cc6
Dan Weikart <danmweikart@gmail.com>
2020-05-27 07:26:24 -0700

    updated burger_controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -2,4 +2,12 @@ const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger.js");
 
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        
+    })
+})
+
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
-DROP DATABASE IF EXISTS burgers_db;
+
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
 id INTEGER(11) AUTO_INCREMENT NOT NULL,
 burger_name VARCHAR(30) NOT NULL,
-devoured BOOLEAN NOT NULL,
+devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
